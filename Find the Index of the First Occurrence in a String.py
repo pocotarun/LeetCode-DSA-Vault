8026cf5a5622bfc246a1b3f30ab77bb817f240d7
Tarun <engr.tarundilwal@gmail.com>
2026-07-19 22:55:12 +0530

    Add TypeScript implementation for expression types and functions with numeric and text expressions

--- Find the Index of the First Occurrence in a String.py
+++ Find the Index of the First Occurrence in a String.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Example 1:
 Input: haystack = "sadbutsad", needle = "sad"
 Output: 0
@@ -9,16 +9,12 @@ Example 2:
 Input: haystack = "leetcode", needle = "leeto"
 Output: -1
 Explanation: "leeto" did not occur in "leetcode", so we return -1.
- """
+"""
 
-
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.index(needle)
-        # return  0
+# return  0
 
 # dont read only for test
 sol = Solution()
 haystack = "sadbutsad"
 needle = "sad"
-print(sol.strStr(haystack , needle))
+print(sol.strStr(haystack, needle))
--- MoveZeroes.py
+++ MoveZeroes.py
@@ -1,11 +1,12 @@
-nums = [0,1,0,3,12]
-def moveZeroes(self, nums):
+nums = [0, 1, 0, 3, 12]
+
+
+def moveZeroes(nums):
     count = nums.count(0)
-    while 0 in nums :
+    while 0 in nums:
         nums.remove(0)
 
-
-    for count in range(count):
+    for _ in range(count):
         nums.append(0)
     return nums
 
